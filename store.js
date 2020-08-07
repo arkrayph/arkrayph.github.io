1c6f03ccedf9438b622b11203d43327d54fe4612
arkrayph <68732956+arkrayph@users.noreply.github.com>
2020-08-07 08:13:23 +0800

    Update store.js

--- store.js
+++ store.js
@@ -27,12 +27,21 @@ function ready() {
 }
 
 function purchaseClicked() {
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+    document.getElementById("timeStamp").value = date+' '+time;
+    document.getElementById("ordered_items").value = document.getElementsByClassName('cart-items')[0].innerText
+    document.getElementById("total_price").value = document.getElementsByClassName('cart-total-price')[0].innerText;
+
+    // Submit the form
     alert('Thank you for your purchase')
     var cartItems = document.getElementsByClassName('cart-items')[0]
     while (cartItems.hasChildNodes()) {
         cartItems.removeChild(cartItems.firstChild)
     }
-    updateCartTotal()
+    updateCartTotal() 
 }
 
 function removeCartItem(event) {
@@ -71,15 +80,15 @@ function addItemToCart(title, price, imageSrc) {
         }
     }
     var cartRowContents = `
-        <div class="cart-item cart-column">
-            <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
-            <span class="cart-item-title">${title}</span>
-        </div>
-        <span class="cart-price cart-column">${price}</span>
-        <div class="cart-quantity cart-column">
-            <input class="cart-quantity-input" type="number" value="1">
-            <button class="btn btn-danger" type="button">X</button>
-        </div>`
+<div class="cart-item cart-column">
+<img class="cart-item-image" src="${imageSrc}" width="100" height="100">
+<span class="cart-item-title">${title}</span>
+</div>
+<span class="cart-price cart-column">${price}</span>
+<div class="cart-quantity cart-column">
+<input class="cart-quantity-input" type="number" value="1">
+<button class="btn btn-danger" type="button">X</button>
+</div>`
     cartRow.innerHTML = cartRowContents
     cartItems.append(cartRow)
     cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
